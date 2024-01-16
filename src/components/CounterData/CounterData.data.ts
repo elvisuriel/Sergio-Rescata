8574abddade90ec0c3caf8b6b0700cfb61e199e4
elvisuriel <urielmarciales@gmil.com>
2024-01-15 22:23:24 -0500

    subiendo mas archivos

---
+++ src/components/CounterData/CounterData.data.ts
@@ -0,0 +1,20 @@
+export const dataCounter = [
+  {
+    id: 1,
+    startNumber: 500,
+    endNumber: 3800,
+    text: "Dane",
+  },
+  {
+    id: 2,
+    startNumber: 0,
+    endNumber: 230,
+    text: "mas estadisticas ",
+  },
+  {
+    id: 3,
+    startNumber: 600,
+    endNumber: 1800,
+    text: "otras estadisticas",
+  },
+];
---
+++ src/components/FirstBlock/FirstBlock.tsx
@@ -0,0 +1,57 @@
+"use client";
+import Link from "next/link";
+import { BackgroundRadialRight } from "../BackgroundRadialRight";
+import { MotionTransition } from "../MotionTransition";
+import Image from "next/image";
+import { Reveal } from "../Reveal";
+
+export function FirstBlock() {
+  return (
+    <div className="relative p-4 md:py-20">
+      <BackgroundRadialRight />
+      <div className="grid max-w-5xl mx-auto md:grid-cols-2">
+        <div>
+          <Reveal>
+            <h1 className="text-5xl font-semibold">
+              Sergio-Rescata
+              <span className="block degradedBlue bg-blueLight">
+                La salud mental
+              </span>
+              es una prioridad
+            </h1>
+          </Reveal>
+          <Reveal>
+            <p className="max-w-md mt-10">
+              Somos.... Cucuta que desde hace 3 años trabaja en la salud,
+              intervención, acompañamiento y formación a nivel personal,
+              familiar, institucional, comunitario y empresarial apoyando a los
+              jóvenes y adultos a superar situaciones de adicción, vulneración
+              de derechos y dependencia de sustancias. Con nuestra intervención
+              planeamos y seguimos una ruta de trabajo y atención para ayudarlos
+              a definir un proyecto de vida y así superar cada situación.
+            </p>
+          </Reveal>
+          <Reveal>
+            <div className="my-8">
+              <Link
+                href="#clients"
+                className="px-4 py-3 rounded-md bg-blueRadial"
+              >
+                Empieza ahora
+              </Link>
+            </div>
+          </Reveal>
+        </div>
+        <MotionTransition className="flex items-center justify-center">
+          <Image
+            src="/assets/cards-block.PNG"
+            alt="Card"
+            width={450}
+            height={450}
+            className="h-auto w-72 md:w-full"
+          />
+        </MotionTransition>
+      </div>
+    </div>
+  );
+}
