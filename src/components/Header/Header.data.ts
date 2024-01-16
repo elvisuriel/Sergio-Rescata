fbe654ad4de2592d3ed484b0a457428b3ab3d099
ELVIS URIEL MARCIALES <100080057+elvisuriel@users.noreply.github.com>
2024-01-15 22:46:48 -0500

    Add files via upload
    nuevos componentes

---
+++ src/components/Header/Header.data.ts
@@ -0,0 +1,22 @@
+export const dataHeader = [
+  {
+    id: 1,
+    name: "Inicio",
+    idLink: "#features",
+  },
+  {
+    id: 2,
+    name: "Estadísticas",
+    idLink: "#tarjetas",
+  },
+  {
+    id: 3,
+    name: "Testimonios",
+    idLink: "#testimonios",
+  },
+  {
+    id: 4,
+    name: "Servicios",
+    idLink: "#servicios",
+  },
+];
