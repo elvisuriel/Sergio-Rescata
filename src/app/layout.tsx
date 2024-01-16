c53264df356d625d49a4f654f8121c2bea1db7d8
elvisuriel <urielmarciales@gmil.com>
2024-01-15 22:20:10 -0500

    firist commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+const popinsFont = Poppins({
+  weight: ["400", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Sergio-Rescata",
+  description: "Landing Page sobre proyecto social en la ciudad de Cucuta",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body className={popinsFont.className}>{children}</body>
+    </html>
+  );
+}
